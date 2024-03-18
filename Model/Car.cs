209a33f9acadd8a17896d5adfa40899aabff5d67
raddevus <1radeutsch1@gmail.com>
2024-03-18 18:54:40 -0400

    Added Car class to have data to write to sqlite db.

---
+++ Model/Car.cs
@@ -0,0 +1,6 @@
+namespace sqliteThreads.Model;
+class Car{
+    public string Make{get;set;}
+    public string Model{get;set;}
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using sqliteThreads.Model;
+
 Console.WriteLine("Hello, World!");
+
+Car c = new Car{Make="Ford", Model="Fairlane"};
+
+Console.WriteLine($"{c.Make} : {c.Model}");
