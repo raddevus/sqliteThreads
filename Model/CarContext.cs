fec17d59a18f98b7573d4fc5fb6b0c404fcf2b59
raddevus <1radeutsch1@gmail.com>
2024-03-18 19:20:54 -0400

    CarContext class now creates the car.db file (if it doesn't exist) and adds the vehicle table which we'll use for our insert tests

--- Model/CarContext.cs
+++ Model/CarContext.cs
@@ -3,6 +3,7 @@ namespace sqliteThreads.Model;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
 
 public class CarContext : DbContext
 {
@@ -16,6 +17,28 @@ public class CarContext : DbContext
         var path = Environment.GetFolderPath(folder);
         DbPath = System.IO.Path.Join(path, "car.db");
         Console.WriteLine(DbPath);
+
+        SqliteConnection connection = new SqliteConnection($"Data Source={DbPath}");
+        // ########### FYI THE DB is created when it is OPENED ########
+        connection.Open();
+        SqliteCommand command = connection.CreateCommand();
+        FileInfo fi = new FileInfo(DbPath);
+        if (fi.Length == 0){
+            foreach (String tableCreate in allTableCreation){
+                command.CommandText = tableCreate;
+                command.ExecuteNonQuery();
+            }
+        }
+        Console.WriteLine(connection.DataSource);
     }
+
+    protected String [] allTableCreation = {
+        @"CREATE TABLE vehicle
+            ( [ID] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            [Make] NVARCHAR(350)  NOT NULL check(length(Make) <= 350),
+            [Model] NVARCHAR(350)  NOT NULL check(length(Model) <= 350),
+            [Year] INTEGER NOT NULL default 0
+            )"
+    };
 
 }
